88a85100b67754f473731837805cfc190dfea0e3
Sangappa-123 <birajsd@gmail.com>
2024-12-19 12:36:21 +0530

    Create main.tf

---
+++ multitenancy/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-1212"
+    key            = "multitenancy/${terraform.workspace}/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-app-${terraform.workspace}-bucket-1212"
+  acl    = "private"
+}
